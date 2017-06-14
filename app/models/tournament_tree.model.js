b9ca6b54b6a9fd0e49cd3d3cebfbfaa08dc35fe5
fblost <fblost@gmail.com>
2017-06-14 19:18:09 +0200

    feat(Model): Created tree model

--- app/models/tournament_tree.model.js
+++ app/models/tournament_tree.model.js
@@ -6,12 +6,62 @@ module.exports = function (mongoose) {
     
   var Schema = new mongoose.Schema({
       
-    Name: {
+    TreeName: {
         type: Types.String,
         required: true,
         unique: true
-    },                               
-    
+    },
+
+      /*
+       * MongoDB automatically created _id fields for each element in subarray
+       * we use this _id field to reference participants from tree nodes
+       */
+    Participants: [ {
+        DisplayName: {
+            type: Types.String,
+        },
+
+        EmailAddress: {
+            type: Types.String,
+            required: true,
+            unique: true
+        },
+
+    }],
+
+    TreeNodes: [ {
+
+        Player1_id: {
+            type: Types.String,
+        },
+
+        Player2_id: {
+            type: Types.String,
+        },
+
+        /*
+            0 - undecided
+            1 - player 1 won
+            2 - player 2 won
+         */
+        Result: {
+            type: Types.Number,
+            default: 0
+        },
+
+        Note: {
+            type: Types.String
+        },
+
+        /*
+         * _id of the next node.
+         *  Last node has this set to empty string
+         */
+        NextTreeNode_id: {
+            type: Types.String
+        }
+
+    } ]
         
   });
   
