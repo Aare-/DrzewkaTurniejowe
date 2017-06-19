93c9620895aab183642a3a96c4325c4e8bb471bc
jakubwida <jakub.wida@outlook.com>
2017-06-19 13:15:42 +0200

    routing troubles continue

--- angular-quickstart/app/app.component.ts
+++ angular-quickstart/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>
-	<login-info><login-info>
+   <nav>
+     <a routerLink="/home">home</a>
+     <a routerLink="/browse">browse</a>
+   </nav>
 	<router-outlet></router-outlet>
+	<login-info><login-info>
+
+
 
 	`,
 })
